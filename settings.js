b348526da9749f737304adfad915aa030fcfd780
eranga-nuwan <eranga.nuwan132@gmail.com>
2025-10-02 21:00:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "3J5HHILI#VvBCqy0S3naSxfMeQU12Hd1zYqIYEI53ZFhkOW0Wva8, // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
